ea41e6b8c0ecf873b815836a337d21b4f807c769
Blair Duddy <blairduddy@hotmail.co.uk>
2023-05-23 17:38:35 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Blair Duddy on 2023-05-23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
